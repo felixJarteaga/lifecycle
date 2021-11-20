e05a5d69c37e9bfca86dbe97e5a5674e00b16682
Felix José Arteaga trigo <farteaga21791@gmail.com>
2021-11-20 11:59:24 +0100

    fin sección 20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Pagina1Component
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Pagina1Component, MuestraNombreComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
